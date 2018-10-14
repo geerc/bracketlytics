682b305a96331b1dd7a7eaa2d5e8feb8d1f0451f
Christian Geer <geerc@allegheny.edu>
2018-10-14 16:45:22 -0400

    Deleted code that is handled by data_cleaning.r

--- fourFactors.py
+++ fourFactors.py
@@ -1,36 +1,11 @@
 import pandas as pd
 import numpy as np
 
-csv_path = '/Users/Christian/Documents/Bracketlytics/data/2018TeamStats_Final.csv'
+csv_path = '/Users/Christian/Documents/Bracketlytics/data/season_data.csv'
 sos_path = '/Users/Christian/Documents/Bracketlytics/data/bf.csv'
-df = pd.read_csv(csv_path)
 sos = pd.read_csv(bf_path)
+df2 = pd.read_csv(csv_path)
 
-# Drop uncessary columns
-df = df.drop(['FG%', '3P%', '2P', '2PA', '2P%', 'TRB', 'AST'], axis = 1)
-df = df.drop(['STL', 'BLK', 'PF', 'PTS', 'Opp FG%', 'Opp 2P', 'Opp 2PA'], axis = 1)
-df = df.drop(['Opp 2P%', 'Opp TRB', 'Opp AST', 'Opp STL', 'Opp BLK', 'Opp PF', 'Opp PTS'], axis = 1)
-df = df.drop(['FT%', 'Opp FT%', 'Opp 3P%', 'MP', 'Win?', 'gameid', '3PA'], axis = 1)
-df = df.drop(['Opp 3PA'], axis = 1)
-df[0:5]
-
-# Convert data from objects to floats
-df[['FG']] = df[['FG']].apply(pd.to_numeric, errors = 'corerce')
-df[['FGA']] = df[['FGA']].apply(pd.to_numeric, errors = 'corerce')
-df[['3P']] = df[['3P']].apply(pd.to_numeric, errors = 'corerce')
-df[['FT']] = df[['FT']].apply(pd.to_numeric, errors = 'corerce')
-df[['ORB']] = df[['ORB']].apply(pd.to_numeric, errors = 'corerce')
-df[['DRB']] = df[['DRB']].apply(pd.to_numeric, errors = 'corerce')
-df[['TOV']] = df[['TOV']].apply(pd.to_numeric, errors = 'corerce')
-df[['FTA']] = df[['FTA']].apply(pd.to_numeric, errors = 'corerce')
-df[['Opp FG']] = df[['Opp FG']].apply(pd.to_numeric, errors = 'corerce')
-df[['Opp FGA']] = df[['Opp FGA']].apply(pd.to_numeric, errors = 'corerce')
-df[['Opp 3P']] = df[['Opp 3P']].apply(pd.to_numeric, errors = 'corerce')
-df[['Opp FT']] = df[['Opp FT']].apply(pd.to_numeric, errors = 'corerce')
-df[['Opp ORB']] = df[['Opp ORB']].apply(pd.to_numeric, errors = 'corerce')
-df[['Opp DRB']] = df[['Opp DRB']].apply(pd.to_numeric, errors = 'corerce')
-df[['Opp TOV']] = df[['Opp TOV']].apply(pd.to_numeric, errors = 'corerce')
-df[['Opp FTA']] = df[['Opp FTA']].apply(pd.to_numeric, errors = 'corerce')
 
 # Create effective field goal percentage for offense and defense
 df['eFG'] = (df['FG'] + float(0.5) * df['3P']) / df['FGA']
@@ -53,6 +28,8 @@ df['oFTp'] = df['Opp FT'] / df['Opp FGA']
 df[['FTp', 'oFTp']] = df[['FTp', 'oFTp']].round(3)
 bf[0:5]
 
+
+df2
 # Create column for team score
 bf['score'] = 0
 
