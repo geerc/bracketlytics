8f5956bb77936e3ea6af1e641edcebf01a4b5e43
Christian Geer <geerc@allegheny.edu>
2018-10-15 12:59:58 -0400

    finished adding strength of schedule to season data

--- data_cleaning.R
+++ data_cleaning.R
@@ -7,13 +7,6 @@ library(tidyverse)
 data = read.csv(file = '/Users/Christian/Documents/Bracketlytics/data/2018TeamStats_Final.csv', header = TRUE, sep = ",")
 sos.data = read.csv(file = '/Users/Christian/Documents/Bracketlytics/data/sos.csv')
 
-sos.data$SOS <- as.numeric(as.character(sos.data$SOS))
-sos.data$Rank <- as.numeric(as.character(sos.data$Rank))
-names(sos.data)[names(sos.data) == "School"] <- "Team"
-
-sos.data$Team <- tolower(sos.data$Team)
-full_data = full_join(season.data, sos.data, by = "Team")
-
 ####Compiling game data to season daata####
 # Remove uncessary columns
 data$gameid <- NULL
@@ -116,3 +109,14 @@ write.csv(sos.data, file = '/Users/Christian/Documents/Bracketlytics/data/sos.cs
 ####Joining data####
 sos.data$Team <- tolower(sos.data$Team)
 full_data = full_join(season.data, sos.data, by = "Team")
+
+sos.data$SOS <- as.numeric(as.character(sos.data$SOS))
+sos.data$Rank <- as.numeric(as.character(sos.data$Rank))
+names(sos.data)[names(sos.data) == "School"] <- "Team"
+
+sos.data$Team <- tolower(sos.data$Team)
+full_data = right_join(season.data, sos.data, by = "Team")
+
+full_data <- na.omit(full_data)
+
+write.csv(sos.data, file = '/Users/Christian/Documents/Bracketlytics/data/season_data.csv')
