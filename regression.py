609644b4e1a486922832987c2dc7162f7987d6f7
Christian Geer <geerc@allegheny.edu>
2018-10-17 15:19:14 -0400

    added 2017-18 off data and deleted strength of schedule

--- regression.py
+++ regression.py
@@ -1,4 +1,8 @@
-import pandas as import pd
+import pandas as pd
 import numpy as np
 
-data_2017_2018 = pd.read.csv("/Users/Christian/Documents/Bracketlytics/data/team_data_17-18")
+csv_path = '/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2017-18.csv'
+data_2017_2018 = pd.read_csv(csv_path)
+del data_2017_2018['SOS']
+
+data_2017_2018
