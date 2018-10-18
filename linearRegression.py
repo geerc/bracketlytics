9bb4ba17407dcac90c360b052cf4c03e904dbe8c
Christian Geer <geerc@allegheny.edu>
2018-10-18 18:50:26 -0400

    Deleted code that is being handled in datacleaning.R

--- linearRegression.py
+++ linearRegression.py
@@ -1,16 +1,4 @@
 import pandas as pd
 import numpy as np
 
-# Loading offensive data
-Off_2017_2018 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2017-18.csv')
-Off_2016_2017 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2016-17.csv')
-Off_2015_2016 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2015-16.csv')
-Off_2014_2015 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2014-15.csv')
-Off_2013_2014 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2013-14.csv')
-
-# Loading defensive data
-Def_2017_2018 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2017-18.csv')
-Def_2016_2017 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2016-17.csv')
-Def_2015_2016 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2015-16.csv')
-Def_2014_2015 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2014-15.csv')
-Def_2013_2014 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2013-14.csv')
+# Loading data
