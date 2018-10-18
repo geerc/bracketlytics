ef1ec7ba1e8839a498e18c9f70dc79828092195f
Christian Geer <geerc@allegheny.edu>
2018-10-18 18:26:59 -0400

    code to load data

--- linearRegression.py
+++ linearRegression.py
@@ -1,8 +1,16 @@
 import pandas as pd
 import numpy as np
 
-csv_path = '/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2017-18.csv'
-data_2017_2018 = pd.read_csv(csv_path)
-del data_2017_2018['SOS']
+# Loading offensive data
+Off_2017_2018 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2017-18.csv')
+Off_2016_2017 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2016-17.csv')
+Off_2015_2016 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2015-16.csv')
+Off_2014_2015 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2014-15.csv')
+Off_2013_2014 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Off_2013-14.csv')
 
-data_2017_2018
+# Loading defensive data
+Def_2017_2018 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2017-18.csv')
+Def_2016_2017 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2016-17.csv')
+Def_2015_2016 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2015-16.csv')
+Def_2014_2015 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2014-15.csv')
+Def_2013_2014 = pd.read_csv('/Users/Christian/Documents/Bracketlytics/data/model_data/Def_2013-14.csv')
