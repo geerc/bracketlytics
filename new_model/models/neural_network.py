8570daf740fdc415170ec3867e901fe45be164c9
Christian Geer <geerc@allegheny.edu>
2019-04-19 13:20:27 -0400

    new data and defining base model

--- new_model/models/neural_network.py
+++ new_model/models/neural_network.py
@@ -11,9 +11,19 @@ from sklearn.pipeline import Pipeline
 ROOT = '/Users/Christian/Documents/GitHub/bracketlytics/new_model/'
 
 # load dataset
-dataframe = pd.read_csv(ROOT + 'data/model_data')
+dataframe = pd.read_csv(ROOT + 'data/joined_data.csv')
 dataset = dataframe.values
 
 # split into input (X) and output (Y) variables
-X = dataset[:,0:13]
-Y = dataset[:,13]
+X = dataset[:,1:9]
+Y = dataset[:,10]
+
+# define base model
+def baseline_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(13, input_dim=13, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam')
+	return model
