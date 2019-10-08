e618489ffe9aeca022d725fdc9566774a6f00a80
Christian Geer <geerc@allegheny.edu>
2019-10-08 09:14:52 -0400

    removed some columns

--- data/kaggle_data/data2/adjust_win_column.py
+++ data/kaggle_data/data2/adjust_win_column.py
@@ -3,9 +3,11 @@ import numpy as np
 
 ROOT = '/Users/christiangeer/bracketlytics/data/kaggle_data/data2/'
 
-tourny_data = pd.read_csv(ROOT + 'tourny_games_clean.csv')
+tourny_data = pd.read_csv(ROOT + 'tourney_games_wsuffix.csv')
 print(tourny_data)
 
-tourny_data['win'] = tourny_data['win'].astype(int)
+# tourny_data['win'] = tourny_data['win'].astype(int)
 
-tourny_data.to_csv(ROOT + 'tourny_data_10')
+tourny_data.drop(tourny_data[['Unnamed: 0', 'Unnamed: 0.1','Year']], inplace=True, axis=1)
+
+tourny_data.to_csv(ROOT + 'tourny_data_10.csv')
