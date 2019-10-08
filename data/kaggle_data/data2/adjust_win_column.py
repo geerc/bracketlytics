b2928c801cdbd9bbc35540a5b9b85c5e93e1f2c2
Christian Geer <geerc@allegheny.edu>
2019-10-08 09:10:06 -0400

    removed unnecessary line of code to cast boolean as int

--- data/kaggle_data/data2/adjust_win_column.py
+++ data/kaggle_data/data2/adjust_win_column.py
@@ -4,8 +4,7 @@ import numpy as np
 ROOT = '/Users/christiangeer/bracketlytics/data/kaggle_data/data2/'
 
 tourny_data = pd.read_csv(ROOT + 'tourny_games_clean.csv')
-
-tourny_data['win'] = int(tourny_data['win'])
+print(tourny_data)
 
 tourny_data['win'] = tourny_data['win'].astype(int)
 
