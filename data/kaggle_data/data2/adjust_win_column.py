7d2359aa6b6f11a6c8ab9f200a87ca3248a37372
Christian Geer <geerc@allegheny.edu>
2019-10-08 12:38:57 -0400

    fix suffix on teams

--- data/kaggle_data/data2/adjust_win_column.py
+++ data/kaggle_data/data2/adjust_win_column.py
@@ -3,11 +3,17 @@ import numpy as np
 
 ROOT = '/Users/christiangeer/bracketlytics/data/kaggle_data/data2/'
 
-tourny_data = pd.read_csv(ROOT + 'tourney_games_wsuffix.csv')
-print(tourny_data)
+tourny_data = pd.read_csv(ROOT + 'tourny_games_clean.csv')
+# print(tourny_data)
 
-# tourny_data['win'] = tourny_data['win'].astype(int)
+tourny_data['win'] = tourny_data['win'].astype(int)
 
-tourny_data.drop(tourny_data[['Unnamed: 0', 'Unnamed: 0.1','Year']], inplace=True, axis=1)
+# tourny_data.drop(tourny_data[['Unnamed: 0', 'Unnamed: 0.1','Year']], inplace=True, axis=1)
+
+# Add year suffix
+tourny_data['Year'] = tourny_data['Year'].astype(str)
+tourny_data['Team'] = tourny_data['Team'] + "_" + tourny_data['Year']
+tourny_data['Team 1'] = tourny_data['Team 1'] + "_" + tourny_data['Year']
+
 
 tourny_data.to_csv(ROOT + 'tourny_data_10.csv')
