8a93f551503d6e412c6860a34ba475ff732f18fc
Christian Geer <geerc@allegheny.edu>
2021-03-24 16:41:29 -0400

    using .nunique to get unique number of occurence of each column to show how many times ID occurs in W column

--- March_Mania_2021/data/count_wins.py
+++ March_Mania_2021/data/count_wins.py
@@ -4,5 +4,9 @@ import numpy as np
 root = '/Users/christiangeer/bracketlytics/March_Mania_2021/'
 results = pd.read_csv(root + 'data/MNCAATourneyCompactResults.csv')
 
-results_grouped = results.groupby(by='Season')
+# results.groupby(['Season']).mean()
+results_grouped = pd.MultiIndex.from_frame(results)
+
+results_grouped = results.groupby(['Season',"WTeamID"]).nunique()
+
 print(results_grouped)
