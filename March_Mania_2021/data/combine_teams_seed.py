a41070fdb473aab23d6170bacc2d6b2410cd26c7
Christian Geer <geerc@allegheny.edu>
2021-03-24 16:05:53 -0400

    realized I don't have to do this

--- March_Mania_2021/data/combine_teams_seed.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import numpy as np
-
-root = "/Users/christiangeer/bracketlytics/March_Mania_2021/"
-seeds = pd.read_csv(root + "data/MNCAATourneySeeds.csv")
-teams = pd.read_csv(root + "data/MTeams.csv")
