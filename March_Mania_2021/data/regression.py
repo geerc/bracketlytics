5f3c945313270bbeb4f013e787692e58d96445c3
Christian Geer <geerc@allegheny.edu>
2021-03-30 10:36:52 -0400

    adding regression and scaling cur tourn data

--- March_Mania_2021/data/regression.py
+++ March_Mania_2021/data/regression.py
@@ -5,7 +5,7 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import statsmodels.api as sm
-from sklearn import datasets, linear_model
+from sklearn import datasets, linear_model, preprocessing
 from sklearn.metrics import mean_squared_error, r2_score
 
 root = '/Users/christiangeer/bracketlytics/March_Mania_2021/'
@@ -18,7 +18,6 @@ curr_tourn = pd.read_csv(root + 'data/curr_tourn.csv')
 hist_bbref = hist_bbref.drop(columns=['Unnamed: 0'], axis=1)
 hist_tourn = hist_tourn.drop(columns=['Unnamed: 0'], axis=1)
 curr_tourn = curr_tourn.drop(columns=['Unnamed: 0'], axis=1)
-curr_tourn
 
 # Merge the data on the school name
 data = hist_bbref.merge(hist_tourn, on='School')
@@ -36,36 +35,47 @@ X = data.drop(columns=['School','Wins','Seed'])
 # train test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = .3, random_state=25)
 
-print(type(X_train))
-
 # standardize X data
 scaler = preprocessing.StandardScaler().fit(X)
 X_scaled = scaler.transform(X)
 
+# scale curr tourn data the same as hist data
+curr_tourn_sc = scaler.transform(curr_tourn.iloc[:,1:])
+print(curr_tourn_sc)
+
+
+## possible pipe for future
 # pipe = make_pipeline(StandardScaler(), LinearRegression())
 # pipe.fit(X, y)
 #
 # pipe.score(curr_tourn)
 
+# Linear Regression
+LinReg = LinearRegression()
+LinReg.fit(X_train, y_train)
+result = LinReg.score(X_test, y_test)
+print("Accuracy: %.2f%%" % (result*100.0))
 
-model = sm.OLS(y, X).fit()
 
-predictions = model.predict(X)
 
-model.summary()
-
-# Sci kit model prediction
-
-# Create and then fit the model
-lm = linear_model.LinearRegression()
-model = lm.fit(X,y)
-
+# model = sm.OLS(y, X).fit()
 #
-predictions = lm.predict(X)
-# print(predictions)[0:5]
-print(predictions)
-
-print(lm.score(X,y))
+# predictions = model.predict(X)
+#
+# model.summary()
+#
+# # Sci kit model prediction
+#
+# # Create and then fit the model
+# lm = linear_model.LinearRegression()
+# model = lm.fit(X,y)
+#
+# #
+# predictions = lm.predict(X)
+# # print(predictions)[0:5]
+# print(predictions)
+#
+# print(lm.score(X,y))
 
 
 
