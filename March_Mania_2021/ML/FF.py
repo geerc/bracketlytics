2a0253a90763f1164e241f8b49a6f2f8dd5ab0de
Christian Geer <geerc@allegheny.edu>
2021-04-06 10:19:19 -0400

    merging FF in the file and removing weird columm

--- March_Mania_2021/ML/FF.py
+++ March_Mania_2021/ML/FF.py
@@ -2,6 +2,12 @@ import pandas as pd
 
 root = '/Users/christiangeer/bracketlytics/March_Mania_2021/'
 
+opp_stats = pd.read_csv(root + 'data/opp_stats_2010_2020.csv')
+team_stats = pd.read_csv(root + 'data/team_stats_2010_2020.csv')
+tourn_stats = pd.read_csv(root + 'data/tourn_stats_2021.csv')
+tourn_opp_stats = pd.read_csv(root + 'data/tourn_opp_stats_2021.csv')
+
+
 def four_factor(data, opp_data):
     data = data.drop(columns=['G','W','L','W-L%','\xa0','W.1','L.1','\xa0.1','W.2','L.2','\xa0.2','W.3','L.3','\xa0.3','Tm.','Opp.','\xa0.4','MP','FG%','3P%','FT%','AST','STL','BLK','PF'])
     opp_data = opp_data.drop(columns=['G','W','L','W-L%','\xa0','W.1','L.1','\xa0.1','W.2','L.2','\xa0.2','W.3','L.3','\xa0.3','Tm.','Opp.','\xa0.4','MP','FG%','3P%','FT%','AST','STL','BLK','PF'])
@@ -30,3 +36,14 @@ def four_factor(data, opp_data):
     data = data.drop(data.iloc[:,4:13], axis=1)
 
     return data
+
+# Create four factor data
+FF_team = four_factor(team_stats, opp_stats)
+FF_tourn = four_factor(tourn_stats, tourn_opp_stats)
+
+FF_team = FF_team.drop(columns=['Unnamed: 0'], axis=1)
+FF_tourn = FF_tourn.drop(columns=['Unnamed: 0'], axis=1)
+
+
+FF_team.to_csv(root + 'data/FF_team.csv')
+FF_tourn.to_csv(root + 'data/FF_2021tourn.csv')
